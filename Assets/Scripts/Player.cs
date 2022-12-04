a5101b77d69bfd1e34b6483f47edc5738ffd1818
Gabriel <gabrieldubois.eng@gmail.com>
2022-12-04 13:27:05 -0500

    adding bullet

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,18 +9,10 @@ public class Player : MonoBehaviour
     public float speed;
     Rigidbody2D rigid;
     public GameObject prefab;
-
-    private Animator _anim;
-    bool AlmostEquals(double double1, double double2, double precision)
-    {
-        return (Math.Abs(double1 - double2) <= precision);
-    }
-    
     // Start is called before the first frame update
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
-        _anim = GetComponent<Animator>();
     }
 
     void FixedUpdate()
@@ -33,30 +24,10 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        input.x = Input.GetAxisRaw("Horizontal");
-        input.z = Input.GetAxisRaw("Vertical");
-        // Debug.Log(AlmostEquals(input.x, 0.0f, 0.00005));
-        // Debug.Log(input.z);
-        // Debug.Log(input.x != 0 || input.z != 0);
-        if ((AlmostEquals(input.x, 0.1f, 0.00005) && AlmostEquals(input.x, 0.1f, 0.00005)))
-            _anim.SetBool("isWalking", false);
-        else
-        {
-            _anim.SetFloat("movX", input.x);
-            _anim.SetFloat("movY", input.z);
-            _anim.SetBool("isWalking", true);
-        }
-
-        rigid.transform.position += new Vector3(input.x, input.z, 0) *speed*Time.deltaTime;
         if(Input.GetKeyDown(KeyCode.Space ))
         {
             GameObject clone = Instantiate(prefab, transform.position, transform.rotation);
         }
 
-
-    }
-    public void Riddle()
-    {
-        Debug.Log("Riddle");
     }
 }
