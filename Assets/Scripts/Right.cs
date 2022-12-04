c2d10cdce8fd8e4e61754ac87a7e7b4774e45d36
rex884 <mikeyrex884@gmail.com>
2022-12-04 17:16:43 -0500

    Narrative stuff

--- Assets/Scripts/Right.cs
+++ Assets/Scripts/Right.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Right : MonoBehaviour
 {
     // Start is called before the first frame update
     public TextMeshProUGUI textComponent;
+    AudioSource source;
+    public AudioClip clip;
     void Start()
     {
-        
+        source = GetComponent<AudioSource>();
+        source.clip = clip;
+        source.Play();
     }
 
     // Update is called once per frame
@@ -21,5 +26,9 @@ public class Right : MonoBehaviour
     public void ChangeScene()
     {
         textComponent.text = "You Win!";
+    }
+    public void ChangeBadScene()
+    {
+        SceneManager.LoadScene("Entry level");
     }
 }
