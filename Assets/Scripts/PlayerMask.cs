aef1032a8f6739c5203ba007488dcdbe0a0d6db5
Alexis <alexis>
2022-12-04 12:56:30 -0500

    add profile mask

--- Assets/Scripts/PlayerMask.cs
+++ Assets/Scripts/PlayerMask.cs
@@ -7,17 +7,40 @@ public class PlayerMask : MonoBehaviour
 {
     [SerializeField]
     public GameObject player;
+
+    [SerializeField]
+    public Sprite profile_mask;
+    public Sprite default_mask;
     
+    
+    private Vector3 scale_profile_mask;
+    private Vector3 scale_default_mask;
     private bool _isTrigger;
     private SpriteRenderer _rd;
 
     private void Start()
     {
+        scale_default_mask = transform.localScale;
+        scale_profile_mask = new Vector3(6.5f, 6.5f, 6.5f);
         _rd = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
     {
+        if (_isTrigger && (Input.GetAxis("Horizontal") > 0 || Input.GetAxis("Horizontal") < 0))
+        {
+            _rd.sprite = profile_mask;
+            transform.localScale = scale_profile_mask;
+            if (Input.GetAxis("Horizontal") < 0)
+                _rd.flipX = true;
+            else
+                _rd.flipX = false;
+        }
+        else
+        {
+            _rd.sprite = default_mask;
+            transform.localScale = scale_default_mask;
+        }
         if (_isTrigger && Input.GetAxis("Vertical") > 0)
             _rd.enabled = false;
         else
@@ -25,6 +48,10 @@ public class PlayerMask : MonoBehaviour
         if (_isTrigger)
         {
             transform.position = player.gameObject.transform.position;
+            if (Input.GetAxis("Horizontal") > 0)
+                transform.position += new Vector3(0.3f, 0, 0);
+            else if (Input.GetAxis("Horizontal") < 0)
+                transform.position += new Vector3(-0.3f, 0, 0);
         }
     }
 
